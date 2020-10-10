8c4a22bd2b8046d7541b5c50ac15694c46d2f2c9
Alex Nielsen <alex.nielsen@gmail.com>
2020-10-10 01:18:39 -0400

    fixed backend link to heroku

--- Frontend/bushbass_frontend/src/App.js
+++ Frontend/bushbass_frontend/src/App.js
@@ -1,30 +1,31 @@
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Axios from 'axios';
-import UserContext from './context/UserContext';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Axios from "axios";
+import UserContext from "./context/UserContext";
 
-import './index.css';
-import Header from './components/Header';
-import Home from './components/Home';
-import Search from './components/Search';
-import Movie from './components/Movie';
-import Login from './components/auth/Login';
-import Register from './components/auth/Register';
-import Favorites from './components/Favorites';
+import "./index.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Search from "./components/Search";
+import Movie from "./components/Movie";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import Favorites from "./components/Favorites";
 
 export default function App() {
   const [movieList, setMovieList] = useState([]);
   const [userData, setUserData] = useState({
     token: undefined,
-    user: undefined,
+    user: undefined
   });
 
-  // `https://hackclub-movie-backend.herokuapp.com` ||
-  const [BACKEND_URL] = useState('http://localhost:5000');
+  const [BACKEND_URL] = useState(
+    `https://hackclub-movie-backend.herokuapp.com` || "http://localhost:5000"
+  );
 
   useEffect(() => {
     fetch(
-      'https://api.themoviedb.org/3/movie/popular?api_key=dff2ace9d4fe143fc9aad06522638b5c'
+      "https://api.themoviedb.org/3/movie/popular?api_key=dff2ace9d4fe143fc9aad06522638b5c"
     )
       .then((response) => response.json())
       .then((data) => setMovieList(data.results));
@@ -32,19 +33,19 @@ export default function App() {
 
   useEffect(() => {
     const checkLoggedIn = async () => {
-      let token = localStorage.getItem('auth-token');
+      let token = localStorage.getItem("auth-token");
       if (token === null) {
-        localStorage.setItem('auth-token', '');
-        token = '';
+        localStorage.setItem("auth-token", "");
+        token = "";
       }
       const tokenResponse = await Axios.post(
         `${BACKEND_URL}/users/tokenIsValid`,
         null,
-        { headers: { 'x-auth-token': token } }
+        { headers: { "x-auth-token": token } }
       );
       if (tokenResponse.data) {
         const userResponse = await Axios.get(`${BACKEND_URL}/users`, {
-          headers: { 'x-auth-token': token },
+          headers: { "x-auth-token": token }
         });
         setUserData({ token, user: userResponse.data });
       }
@@ -54,7 +55,7 @@ export default function App() {
   return (
     <Router>
       <UserContext.Provider value={{ userData, setUserData, BACKEND_URL }}>
-        <div className='App'>
+        <div className="App">
           <Header />
           {!userData.user ? (
             <p>no one logged in</p>
@@ -62,21 +63,21 @@ export default function App() {
             <p>You're logged in {userData.user.displayName}</p>
           )}
           <Switch>
-            <Route path='/search'>
+            <Route path="/search">
               <Search movieList={movieList} />
             </Route>
 
-            <Route path='/login'>
+            <Route path="/login">
               <Login />
             </Route>
-            <Route path='/favorites'>
+            <Route path="/favorites">
               <Favorites />
             </Route>
-            <Route path='/register'>
+            <Route path="/register">
               <Register />
             </Route>
-            <Route path='/:id' children={<Movie />} />
-            <Route path='/'>
+            <Route path="/:id" children={<Movie />} />
+            <Route path="/">
               <Home movieList={movieList} />
             </Route>
           </Switch>
