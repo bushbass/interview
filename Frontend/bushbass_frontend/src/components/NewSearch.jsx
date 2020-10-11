56fc9fa0e6f1999b60c6a60de575b2c127ff727d
Alex Nielsen <alex.nielsen@gmail.com>
2020-10-11 18:23:58 -0400

    got search from API working

---
+++ Frontend/bushbass_frontend/src/components/NewSearch.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect, useContext } from "react";
+import UserContext from "../context/UserContext";
+import { useHistory } from "react-router-dom";
+import "./favorites.css";
+
+function Favorites() {
+  const [inputText, setInputText] = useState("");
+  const { userData } = useContext(UserContext);
+  const [searchResults, setSearchResults] = useState([]);
+  const history = useHistory();
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=dff2ace9d4fe143fc9aad06522638b5c&language=en-US&query=${inputText}&page=1&include_adult=false`
+    ).then((res) => res.json().then((data) => setSearchResults(data.results)));
+  };
+  useEffect(() => {
+    if (!userData.user) history.push("/login");
+  });
+
+  return (
+    <div className="page">
+      <h2> New search</h2>
+      <form onSubmit={submitForm}>
+        <input
+          type="text"
+          value={inputText}
+          onChange={(Event) => setInputText(Event.target.value)}
+        />
+        <button>submit</button>
+      </form>
+
+      {searchResults.map((movie) => console.log(movie))}
+      {searchResults.map((movie) => (
+        <div>
+          {movie.original_title} - {movie.id}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Favorites;
